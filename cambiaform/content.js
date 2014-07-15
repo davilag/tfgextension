8d5d74b43638e24c43388886ecf2ceb4101d3906
davilag <d.avilag23@gmail.com>
2014-07-15 21:51:20 +0200

    Prueba en todas las paginas, falla la primer vez en alguans

--- cambiaform/content.js
+++ cambiaform/content.js
@@ -3,13 +3,15 @@ chrome.runtime.onMessage.addListener(function(request, sender, sendResponse)
 	console.log("Llega algo");
 	console.log(request);
     if (request.greeting == "find_form"){
-    	console.log("Entra");    
+      $(document).ready(function(){
+        console.log("Entra");    
         var inputs = $("input").get().reverse();
         var indice = undefined;
         var passfound = false;
         $(inputs).each(function(index){
             if($(this).attr("type")=="password"){
                 $(this).val("hola");
+                console.log("Tiene un campo pass");
                 passfound = true;
             }else if($(this).attr("type")=="text" || $(this).attr("type")=="email" || $(this).attr("type")==undefined){
                 if(passfound){
@@ -18,5 +20,6 @@ chrome.runtime.onMessage.addListener(function(request, sender, sendResponse)
                 }
             }
         });
+      })
      }  	
 });
