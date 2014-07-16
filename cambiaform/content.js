34fa0e9e92d87fe9a127a2bbd7fd189a52552473
davilag <d.avilag23@gmail.com>
2014-07-16 11:24:48 +0200

    Update content.js
    Funciona cuando no se coge de cache la pagina y cambia el color del formulario para indicar que se ha rellenado.

--- cambiaform/content.js
+++ cambiaform/content.js
@@ -1,25 +1,27 @@
 chrome.runtime.onMessage.addListener(function(request, sender, sendResponse)
 {
-	console.log("Llega algo");
+	console.log("Ejecutando content");
 	console.log(request);
-    if (request.greeting == "find_form"){
-      $(document).ready(function(){
-        console.log("Entra");    
-        var inputs = $("input").get().reverse();
-        var indice = undefined;
-        var passfound = false;
-        $(inputs).each(function(index){
-            if($(this).attr("type")=="password"){
-                $(this).val("hola");
-                console.log("Tiene un campo pass");
-                passfound = true;
-            }else if($(this).attr("type")=="text" || $(this).attr("type")=="email" || $(this).attr("type")==undefined){
-                if(passfound){
-                    passfound = false;
-                    $(this).val("login");
-                }
-            }
-        });
-      })
-     }  	
-});
+    	if (request.greeting == "find_form"){
+      		$(document).ready(function(){
+        		console.log("Entra");    
+		        var inputs = $("input").get().reverse();
+		        var indice = undefined;
+		        var passfound = false;
+        		$(inputs).each(function(index){
+	            		if($(this).attr("type")=="password"){
+	                		$(this).val("hola");
+					$(this).css("background-color","#A2D6E4");
+	                		console.log("Tiene un campo pass");
+	                		passfound = true;
+	            		}else if($(this).attr("type")=="text" || $(this).attr("type")=="email" || $(this).attr("type")==undefined){
+	        			if(passfound){
+	                    			passfound = false;
+						$(this).css("background-color","#A2D6E4");
+	                    			$(this).val("login");
+	                		}
+            			}
+        		});
+		});
+     	}  	
+});	
