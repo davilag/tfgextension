4f2a3a2f77dbb9ea7c40fbf3d29931dd378f63ed
davilag <d.avilag23@gmail.com>
2014-07-17 11:41:25 +0200

    Ordeno los scipts en funciones para mejorar la lectura. Actualizacion de diario.

--- cambiaform/content.js
+++ cambiaform/content.js
@@ -1,34 +1,44 @@
+
+//Funcion para quitar el fondo amarillo de la funcion autofill de Google Chrome.
+function quitarFondoAutofill(){
+   $('input:-webkit-autofill').each(function(){
+      var text = $(this).val();
+      var name = $(this).attr('name');
+      $(this).after(this.outerHTML).remove();
+      $('input[name=' + name + ']').val(text);
+  });
+}
+
+//Funcion para rellenar el formulario de login con el usuario y la contraseña.
+function rellenarForm (login, pass){
+    var inputs = $("input").get().reverse();
+    var indice = undefined;
+    var passfound = false;
+		$(inputs).each(function(index){
+      		if($(this).attr("type")=="password"){
+          		$(this).val(pass);
+	            $(this).css("background-color","#A2D6E4");
+          		passfound = true;
+      		}else if($(this).attr("type")=="text" || $(this).attr("type")=="email" || $(this).attr("type")==undefined){
+  			      if(passfound){
+              		passfound = false;
+		              $(this).css("background-color","#A2D6E4");
+              		$(this).val(login);
+              }
+    			}
+    });
+}
+
+
 chrome.runtime.onMessage.addListener(function(request, sender, sendResponse)
 {
     	if (request.type == "fill_form"){
     	    var login = request.login;
     	    var pass = request.pass;
-    	    /*
-    	    Quitar el fondo amarillo del autofill.
-    	    */
-      	  $('input:-webkit-autofill').each(function(){
-              var text = $(this).val();
-              var name = $(this).attr('name');
-              $(this).after(this.outerHTML).remove();
-              $('input[name=' + name + ']').val(text);
-          });
-          
-		        var inputs = $("input").get().reverse();
-		        var indice = undefined;
-		        var passfound = false;
-        		$(inputs).each(function(index){
-	            		if($(this).attr("type")=="password"){
-	                		$(this).val(pass);
-					            $(this).css("background-color","#A2D6E4");
-	                		passfound = true;
-	            		}else if($(this).attr("type")=="text" || $(this).attr("type")=="email" || $(this).attr("type")==undefined){
-	        			      if(passfound){
-	                    		passfound = false;
-						              $(this).css("background-color","#A2D6E4");
-	                    		$(this).val(login);
-	                    }
-            			}
-		        });
-		        
+
+          quitarFondoAutofill();
+          rellenarForm(login,pass);
+     	}else if(request.type=="look_for_form"){
+     	  
      	}  	
 });	
