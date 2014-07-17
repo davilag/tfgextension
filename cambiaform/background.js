a23b333f313f73619a2860364f3b614db795c94f
davilag <d.avilag23@gmail.com>
2014-07-17 10:26:04 +0200

    Conseguido dominio y paso de parametros por mensaje

--- cambiaform/background.js
+++ cambiaform/background.js
@@ -1,12 +1,23 @@
-// When the extension is installed or upgraded ...
-//chrome.runtime.onInstalled.addListener(function() {
-  // Replace all rules ...
+//Función para obetener el dominio a partir de una url.
+
+function getDominio(url){
+  var dominio = url.match(/^[\w-]+:\/*\[?([\w\.:-]+)\]?(?::\d+)?/)[1];
+  return dominio;
+}
+
+//Listener que salta cuando se cambia el estado de una pestaña
 chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
 
     chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
         if (tabs[0].id === tabId) {
             if (changeInfo && changeInfo.status === 'complete') {
-                chrome.tabs.sendMessage(tabId, {greeting: "find_form"});
+              $.getJSON( "localhost:8080/urls.json", function( data ) {
+                  
+              });
+                var log ="login";
+                var password = "hola";
+                var dominio = getDominio(tabs[0].url);
+                chrome.tabs.sendMessage(tabId, {type: "fill_form",login:log,pass: password});
             }
         }
         else {
@@ -14,5 +25,11 @@ chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
         }
     });
 });
-//});
+
+chrome.runtime.onMessage.addListener(function(request, sender, sendResponse){
+    if (request.greeting == "finished"){
+      alert("Ha llegado algo de vuelta");
+      
+    }
+});
 
--- cambiaform/content.js
+++ cambiaform/content.js
@@ -1,27 +1,34 @@
 chrome.runtime.onMessage.addListener(function(request, sender, sendResponse)
 {
-	console.log("Ejecutando content");
-	console.log(request);
-    	if (request.greeting == "find_form"){
-      		$(document).ready(function(){
-        		console.log("Entra");    
+    	if (request.type == "fill_form"){
+    	    var login = request.login;
+    	    var pass = request.pass;
+    	    /*
+    	    Quitar el fondo amarillo del autofill.
+    	    */
+      	  $('input:-webkit-autofill').each(function(){
+              var text = $(this).val();
+              var name = $(this).attr('name');
+              $(this).after(this.outerHTML).remove();
+              $('input[name=' + name + ']').val(text);
+          });
+          
 		        var inputs = $("input").get().reverse();
 		        var indice = undefined;
 		        var passfound = false;
         		$(inputs).each(function(index){
 	            		if($(this).attr("type")=="password"){
-	                		$(this).val("hola");
-					$(this).css("background-color","#A2D6E4");
-	                		console.log("Tiene un campo pass");
+	                		$(this).val(pass);
+					            $(this).css("background-color","#A2D6E4");
 	                		passfound = true;
 	            		}else if($(this).attr("type")=="text" || $(this).attr("type")=="email" || $(this).attr("type")==undefined){
-	        			if(passfound){
-	                    			passfound = false;
-						$(this).css("background-color","#A2D6E4");
-	                    			$(this).val("login");
-	                		}
+	        			      if(passfound){
+	                    		passfound = false;
+						              $(this).css("background-color","#A2D6E4");
+	                    		$(this).val(login);
+	                    }
             			}
-        		});
-		});
+		        });
+		        
      	}  	
 });	
