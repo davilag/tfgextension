a6b2e8b6f6022f2bc15f5a89c7ac86b67b44dfd7
davilag <d.avilag23@gmail.com>
2014-07-17 11:11:08 +0200

    Conseguido pedir urls, login y pass a un servidor.

--- cambiaform/background.js
+++ cambiaform/background.js
@@ -1,23 +1,37 @@
 //Función para obetener el dominio a partir de una url.
 
 function getDominio(url){
-  var dominio = url.match(/^[\w-]+:\/*\[?([\w\.:-]+)\]?(?::\d+)?/)[1];
+  var urlDom = url.match(/^[\w-]+:\/*\[?([\w\.:-]+)\]?(?::\d+)?/)[1];
+  var urlDomSplit= urlDom.split(".");
+  var dominio = urlDomSplit[urlDomSplit.length-2]+"."+urlDomSplit[urlDomSplit.length-1];
   return dominio;
 }
 
+function sendMessage(dominio,tabId){
+  $.getJSON( "http://localhost:8080/"+dominio+".json?nocache=" + (new Date()).getTime(), function( data ) { //Evitamos que la petición se haga a la cache.
+    var log =data.login;
+    var password = data.pass;
+    chrome.tabs.sendMessage(tabId, {type: "fill_form",login:log,pass: password});
+  });
+
+}
 //Listener que salta cuando se cambia el estado de una pestaña
 chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
 
     chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
         if (tabs[0].id === tabId) {
             if (changeInfo && changeInfo.status === 'complete') {
-              $.getJSON( "localhost:8080/urls.json", function( data ) {
-                  
-              });
-                var log ="login";
-                var password = "hola";
+              $.getJSON( "http://localhost:8080/urls.json?nocache=" + (new Date()).getTime(), function( data ) {//Evitamos que la petición se haga a la cache.
+                console.log("Ha hecho bien la peticion")
                 var dominio = getDominio(tabs[0].url);
-                chrome.tabs.sendMessage(tabId, {type: "fill_form",login:log,pass: password});
+                console.log("el dominio es: "+dominio);
+                for(i = 0; i<data.urls.length;i++){
+                  if(dominio == data.urls[i]){
+                    sendMessage(dominio,tabId);
+                  }
+                }
+              });
+
             }
         }
         else {
@@ -32,4 +46,3 @@ chrome.runtime.onMessage.addListener(function(request, sender, sendResponse){
       
     }
 });
-
